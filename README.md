fe12f7ac5e05aba94a36d359ce86896216691ab3
Michael Dautermann <dautermann-github@spamarrest.com>
2018-09-25 22:48:56 -0400

    makes the tool useable within Xcode

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 genstrings
 ----
 
-Use this script to generate `Localized.strings`.
+Use this script to generate `Localized.strings` via an Xcode "run script" build phase.
 
 ----
 
@@ -21,4 +21,28 @@ Then usage like this.
 
     var myString = "my identifier".localize("my comment")
 
-Run the `main.swift` script of this repository in your root of your application sources, and it will generate a `Localized.strings` to standard out – pipe to desired file.
+What's different between this and the repo I forked from?
+
+Add this script as a "run script" phase directly into your Xcode project (or as a separate .sh file).
+
+1)
+
+Either as a separate, drop in `.swift` file that's called from the Run Script build phase like this:
+![separate .swift script file][swiftscript]
+
+[swiftscript]: https://i.imgur.com/TFFaGwFl.jpg "Separate Swift Script"
+
+or
+
+2)
+
+As an inline script like this:
+
+Make sure to change the "`//`" comment prefixes to script appropriate "`#`"
+
+![inline script][inlinescript]
+
+[inlinescript]: https://i.imgur.com/AslL0uol.jpg "Separate Swift Script"
+
+Also, I'm putting the `Localizable.strings` output into the `SRCROOT` folder, so you'll need to add the file you want to write it to as an Run Script output file destination.
+
--- genstrings/main.swift
+++ genstrings/main.swift
@@ -1,4 +1,4 @@
-#!/usr/bin/swift
+#!/usr/bin/env xcrun --sdk macosx swift
 //
 //  main.swift
 //  genstrings
@@ -8,6 +8,34 @@
 //
 
 import Foundation
+
+/// Exits the script because of an error.
+func fail(_ error: String) -> Never {
+    print("💥  \(error)")
+    exit(-1)
+}
+
+/// Prints an info message.
+func info(_ message: String) {
+    print("ℹ️  \(message)")
+}
+
+/// Prints a success message and exits the script.
+func success(_ message: String) -> Never {
+    print("✅  \(message)")
+    exit(0)
+}
+
+/// Gets an environment variable value with the given name.
+func getEnvironmentValue(key: String) -> String? {
+    guard let rawValue = getenv(key) else {
+        return nil
+    }
+    
+    return String(cString: rawValue)
+}
+
+info("looking for strings to localize")
 
 let exp = try! NSRegularExpression(pattern: "(?<=\")([^\"]*)(?=\".(localize\\((\\\"(.*?)\\\")?\\)))", options: [])
 
@@ -24,45 +52,66 @@ func findFiles(path: String) throws -> [String] {
     })
 }
 
+/// Returns the output file.
+func getOutput() -> URL {
+    guard let plistPath = getEnvironmentValue(key: "SCRIPT_OUTPUT_FILE_0") else {
+        fail("an output file needs to be set.")
+    }
+    
+    return URL(fileURLWithPath: plistPath)
+}
+
 var path: String? = nil
 
-if CommandLine.arguments.count > 1 {
+if (CommandLine.arguments.count > 1) {
     path = CommandLine.arguments[1]
 }
 
-FileHandle.standardOutput.write(Data(bytes: [0xfe, 0xff]))
-FileHandle.standardOutput.write(try findFiles(path: path ?? FileManager.default.currentDirectoryPath)
-    .map { (path) -> [(String, String?)] in
-        
-        let data = try! Data(contentsOf: URL(fileURLWithPath: path))
-        
-        let string = String(data: data, encoding: .utf8)!
-        
-        return exp.matches(in: string, options: [], range: NSMakeRange(0, string.characters.count)).reduce([], { (ret, result) -> [(String, String?)] in
+let outputURL = getOutput()
+
+try? FileManager.default.removeItem(at: outputURL)
+
+do {
+    try "".write(to: outputURL, atomically: true, encoding: .utf16BigEndian)
+    
+    let writeOutput = try FileHandle(forWritingTo: outputURL)
+    writeOutput.write(Data(bytes: [0xfe, 0xff]))
+    writeOutput.write(try findFiles(path: path ?? FileManager.default.currentDirectoryPath)
+        .map { (path) -> [(String, String?)] in
             
-            let found = (0 ..< result.numberOfRanges).map({ (idx) -> String? in
-                let range = result.rangeAt(idx)
-                guard range.location != NSNotFound else { return nil }
-                let startIndex = string.index(string.startIndex, offsetBy: range.location)
-                let endIndex = string.index(startIndex, offsetBy: range.length)
-                return string.substring(with: startIndex ..< endIndex)
+            let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+            
+            let string = String(data: data, encoding: .utf8)!
+            
+            return exp.matches(in: string, options: [], range: NSMakeRange(0, string.count)).reduce([], { (ret, result) -> [(String, String?)] in
+                
+                let found = (0 ..< result.numberOfRanges).map({ (idx) -> String? in
+                    let range = result.rangeAt(idx)
+                    guard range.location != NSNotFound else { return nil }
+                    let startIndex = string.index(string.startIndex, offsetBy: range.location)
+                    let endIndex = string.index(startIndex, offsetBy: range.length)
+                    return String(string[startIndex..<endIndex])
+                })
+                
+                return ret + [(found.first!!, found.last!)]
+                
             })
             
-            return ret + [(found.first!!, found.last!)]
-            
-        })
-        
-    }
-    .reduce([], +)
-    .reduce([]) { (result, strings) -> [(String, String?)] in
-        guard !result.contains(where: { $0.0 == strings.0 }) else { return result }
-        return result + [strings]
-    }
-    .map { (strings) in
-        let string = strings.0.components(separatedBy: "\"").joined(separator: "\\\"")
-        let comment = strings.1 ?? "No comment provided by engineer"
-        return ["", "/* \(comment) */", "\"\(string)\" = \"\(string)\";"]
-    }
-    .reduce([], +)
-    .joined(separator: "\n")
-    .data(using: .utf16BigEndian)!)
+        }
+        .reduce([], +)
+        .reduce([]) { (result, strings) -> [(String, String?)] in
+            guard !result.contains(where: { $0.0 == strings.0 }) else { return result }
+            return result + [strings]
+        }
+        .map { (strings) in
+            let comment = strings.1 ?? "No comment provided by engineer"
+            let string = strings.0.components(separatedBy: "\"").joined(separator: "\\\"")
+            info("\(string) : \(comment)")
+            return ["", "/* \(comment) */", "\"\(string)\" = \"\(string)\";"]
+        }
+        .reduce([], +)
+        .joined(separator: "\n")
+        .data(using: .utf16BigEndian)!)
+} catch let error as NSError {
+    fail("error when opening output - \(error.localizedDescription)")
+}
